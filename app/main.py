5947aedf2a920b1c1468c494bcbf6f1a1570ade4
Naito Hidehiko <hidehiko@7110.email>
2025-05-02 14:27:43 +0900

    update main.py

--- app/main.py
+++ app/main.py
@@ -1,7 +1,46 @@
+import os
+from openai import OpenAI
+from typing import Optional
+from pydantic import BaseModel, Field
 from fastapi import FastAPI
+
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = FastAPI()
 
-@app.get("/")
+OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
+MODEL = 'gpt-4.1-mini'
+
+@app.get('/')
 def read_root():
-    return {"message": "Hello, Oshi-ben 👩"}
+    return {'message': 'Hello, Oshi-ben 👩'}
+
+class SingleChatRequest(BaseModel):
+    system_prompt: Optional[str] = Field(default=None)
+    message: str
+
+class SingleChatResponse(BaseModel):
+    message: str = Field(..., description='可愛い口調で返答する。')
+
+
+@app.post('/v1/single-chat')
+def single_chat_with_openai(payload: SingleChatRequest):
+    client = OpenAI(api_key=OPENAI_API_KEY)
+
+    messages = []
+    if payload.system_prompt:
+        messages.append({'role': 'system', 'content': payload.system_prompt})
+
+    messages.append({'role': 'user', 'content': payload.message})
+
+    completion = client.beta.chat.completions.parse(
+        model=MODEL,
+        messages=messages,
+        response_format=SingleChatResponse,
+    )
+
+    parsed_message = completion.choices[0].message.parsed
+
+    return parsed_message
